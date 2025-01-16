355ef5b00b4fdab522a648d971f9027f5886ce53
RyanHu021 <ryanhu021@gmail.com>
2025-01-15 18:08:08 -0800

    test: switch to screen import

--- test/app/example/page.test.tsx
+++ test/app/example/page.test.tsx
@@ -1,20 +1,20 @@
 /** @jest-environment jsdom */
 import Page from "@/app/example/page";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("Example page", () => {
   it("should render div", () => {
-    const { getByText } = render(<Page />);
+    render(<Page />);
 
-    getByText("This is an example page using App Router!");
+    screen.getByText("This is an example page using App Router!");
   });
 
   // don't actually need this test, but here for demonstration
   it("should not render any buttons", () => {
-    const { queryByRole } = render(<Page />);
+    render(<Page />);
 
-    const button = queryByRole("button");
+    const button = screen.queryByRole("button");
     expect(button).toBeNull();
   });
 });
