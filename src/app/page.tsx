9841417a684a41454bc9eccc7a630d96a51b4497
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2025-01-19 15:05:10 -0800

    tests are passing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <h1>Home</h1>
-    </main>
+    <div className="flex items-center justify-center h-screen">
+      <div className="bold">
+        <Link href="/ivantorriani">Click to Go to Counter </Link>
+      </div>
+    </div>
   );
 }
