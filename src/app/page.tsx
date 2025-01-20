527f77cd312af744de45c3081d5baf7bd4b2ee15
RyanHu021 <ryanhu021@gmail.com>
2025-01-19 22:40:13 -0800

    chore: clean up for merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bold">
-        <Link href="/ivantorriani">Click to Go to Counter </Link>
-      </div>
-    </div>
+    <main>
+      <Navbar />
+      <h1>Home</h1>
+    </main>
   );
 }
