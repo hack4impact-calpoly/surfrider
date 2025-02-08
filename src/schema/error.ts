bbef25b21f36899a78fda681e5f5756527f0bc46
Taliavivrett <taliavivrett@gmail.com>
2025-02-08 12:50:50 -0800

    fix: fixed error names

--- src/schema/error.ts
+++ src/schema/error.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 //errorCode Zod schema
-export const ErrorCodeSchema = z.enum(["CLIENT_ERROR", "API_ERROR", "SERVICE_ERROR"]);
+export const ErrorCode = z.enum(["CLIENT_ERROR", "API_ERROR", "SERVICE_ERROR"]);
 
 //equivalent TypeScript type
-export type ErrorCode = z.infer<typeof ErrorCodeSchema>;
+export type ErrorCode = z.infer<typeof ErrorCode>;
 
 //error Zod schema
-export const ErrorSchema = z.object({
-  code: ErrorCodeSchema,
+export const Error = z.object({
+  code: ErrorCode,
   message: z.string(),
 });
 
 //equivalent TypeScript type
-export type Error = z.infer<typeof ErrorSchema>;
+export type Error = z.infer<typeof Error>;
