8f1fa490f3e60d5f0380d366aa5d585b84e69660
Shayan Daijavad <shayan@daijavad.com>
2025-02-11 09:18:02 -0800

    chore: stopped collecting coverage from src/components/ui

--- jest.config.ts
+++ jest.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/components/ui/**"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
