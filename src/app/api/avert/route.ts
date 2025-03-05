2fb836186d07ca5fe4989cc6cfd5a1c21af5c5ec
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2025-03-04 20:22:36 -0800

    refactor - Delted POST and moved to GET feat - added logic to egrid-fetch.ts

--- src/app/api/avert/route.ts
+++ src/app/api/avert/route.ts
@@ -1,9 +1,63 @@
 import { NextResponse } from "next/server";
-import { fetchAvertData, transformAvertData } from "@/services/avert-fetch";
+import { fetchEgridData, transformEgridData } from "@/services/egrid-fetch";
+import { addEgridRecord, transformEgridRecord } from "@/services/egrid-store";
+//import { AppError, apiErrorHandler } from "@/utils/error";
 
-export async function GET() {
-  const fileBuffer = await fetchAvertData();
-  const transformedData = transformAvertData(fileBuffer);
-  console.log("Cron Job Happened: api/avert");
-  return NextResponse.json({ message: "Cron job executed", success: true, data: transformedData });
-}
+export const GET = async () => {
+  try {
+    const fileBuffer = await fetchEgridData();
+    const transformedData = transformEgridData(fileBuffer);
+
+    if (!Array.isArray(transformedData)) {
+      return NextResponse.json({
+        success: false,
+        error: {
+          code: "CLIENT_ERROR",
+          message: "transformedData needs to be an array",
+        },
+      });
+    }
+
+    for (const record of transformedData) {
+      try {
+        const egridRecord = transformEgridRecord(record);
+        const result = await addEgridRecord(egridRecord);
+
+        if (result && "code" in result) {
+          return NextResponse.json({
+            success: false,
+            error: {
+              code: result.code || "SERVICE_ERROR",
+              message: `Error uploading record with a year ${egridRecord.year} and location ${egridRecord.location}: ${result.message}`,
+            },
+          });
+        }
+      } catch (error) {
+        console.error("Error adding eGRID record:", error);
+        return NextResponse.json({
+          success: false,
+          error: {
+            code: "SERVICE_ERROR",
+            message: "Failed to add eGRID record",
+          },
+        });
+      }
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: "All records uploaded successfully.",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: {
+          code: "API_ERROR",
+          message: `Unexpected error: ${error instanceof Error ? error.message : String(error)}`,
+        },
+      },
+      { status: 500 },
+    );
+  }
+};
