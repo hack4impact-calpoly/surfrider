c905539f292b53d078f34b2d2f06724c471f2b59
nickaan04 <nickaan04@gmail.com>
2025-03-23 14:06:36 -0700

    feat: updated avert route to use error handler

--- src/app/api/avert/route.ts
+++ src/app/api/avert/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 import { fetchAvertData, transformAvertData } from "@/services/avert-fetch";
+import { apiErrorHandler } from "@/utils/errors";
 
 export async function GET() {
-  const fileBuffer = await fetchAvertData();
-  const transformedData = transformAvertData(fileBuffer);
-  console.log("Cron Job Happened: api/avert");
-  return NextResponse.json({ message: "Cron job executed", success: true, data: transformedData });
+  try {
+    const fileBuffer = await fetchAvertData();
+    const transformedData = transformAvertData(fileBuffer);
+    console.log("Cron Job Happened: api/avert");
+    return NextResponse.json({ message: "Cron job executed", success: true, data: transformedData });
+  } catch (error) {
+    return apiErrorHandler(error); //errors are caught and passed here for consistent error response
+  }
 }
