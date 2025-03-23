e0c7988befe214cfc31db99ead608284cafb4f82
nickaan04 <nickaan04@gmail.com>
2025-03-23 14:23:52 -0700

    feat: iterate over objects and add to collection

--- src/app/api/avert/route.ts
+++ src/app/api/avert/route.ts
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
 import { fetchAvertData, transformAvertData } from "@/services/avert-fetch";
+import { addAvertRecord } from "@/services/avert-store";
 import { apiErrorHandler } from "@/utils/errors";
 
 export async function GET() {
   try {
     const fileBuffer = await fetchAvertData();
     const transformedData = transformAvertData(fileBuffer);
+    //iterate over the AvertRecord objects and add them to the database
+    for (const record of transformedData) {
+      await addAvertRecord(record);
+    }
     console.log("Cron Job Happened: api/avert");
     return NextResponse.json({ message: "Cron job executed", success: true, data: transformedData });
   } catch (error) {
