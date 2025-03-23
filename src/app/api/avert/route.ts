a0ed1e3e60f4f71b6e73a347312564277a4fc51d
nickaan04 <nickaan04@gmail.com>
2025-03-23 16:14:22 -0700

    feat: connect to database, update response

--- src/app/api/avert/route.ts
+++ src/app/api/avert/route.ts
@@ -2,8 +2,10 @@ import { NextResponse } from "next/server";
 import { fetchAvertData, transformAvertData } from "@/services/avert-fetch";
 import { addAvertRecord } from "@/services/avert-store";
 import { apiErrorHandler } from "@/utils/errors";
+import connectDB from "@/database/db";
 
 export async function GET() {
+  await connectDB();
   try {
     const fileBuffer = await fetchAvertData();
     const transformedData = transformAvertData(fileBuffer);
@@ -12,7 +14,7 @@ export async function GET() {
       await addAvertRecord(record);
     }
     console.log("Cron Job Happened: api/avert");
-    return NextResponse.json({ message: "Cron job executed", success: true, data: transformedData });
+    return NextResponse.json({ message: "Cron job executed", success: true, data: "successful upload" });
   } catch (error) {
     return apiErrorHandler(error); //errors are caught and passed here for consistent error response
   }
