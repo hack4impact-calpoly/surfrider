b10f2636f5157845f80d14c99c18f88dd078d53e
nickaan04 <nickaan04@gmail.com>
2025-05-01 19:04:45 -0700

    style: default impact field page matches figma

--- src/app/impactfields/page.tsx
+++ src/app/impactfields/page.tsx
@@ -5,19 +5,19 @@ import { ImpactField } from "@/components/impact-field";
 
 export default function Home() {
   return (
-    <div className="max-w-5xl mx-auto p-6 space-y-6">
-      <h1 className="font-bold text-[48px] leading-[48px] tracking-[-1.2] text-[#0F172A]">
+    <div className="max-w-full mx-auto px-14 py-6 space-y-4">
+      <h1 className="font-bold text-[48px] leading-[48px] px-4 tracking-[-1.2] text-[#0F172A]">
         Surfrider Carbon Emissions Calculator
       </h1>
-      <p className="text-gray-600">
+      <p className="font-normal px-4 text-[16px] text-[#64748B]">
         Welcome to the Surfrider Carbon Emissions Calculator! Below, you will find all the key metrics for your project
         based on the data you input into the form. Click the arrows under “Calculated Parameters” to expand and see more
         info related to that metric.
       </p>
 
-      <h2 className="text-2xl font-semibold text-slate-900">Impact Fields</h2>
+      <h2 className="font-semibold px-4 text-[40px] leading-[48px] tracking-[-1.2] text-[#0F172A]">Impact Fields</h2>
 
-      <div className="space-y-4">
+      <div className="space-y-8">
         <ImpactField title="Societal Impact Fields">{/* Societal cards will go here */}</ImpactField>
 
         <ImpactField title="Consumer Impact Fields">{/* Consumer cards will go here */}</ImpactField>
