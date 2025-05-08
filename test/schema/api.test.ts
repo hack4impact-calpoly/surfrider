3513b45906f9d88ace33f18bfc70fc8c337e6aa3
Ryan Chan <ryanchansf@gmail.com>
2025-05-07 21:52:25 -0700

    test: reimplement incorrect input type test

--- test/schema/api.test.ts
+++ test/schema/api.test.ts
@@ -45,4 +45,19 @@ describe("CalculateInput schema", () => {
 
     expect(() => CalculateInput.parse(invalidInput)).toThrow();
   });
+
+  it("should invalidate an incorrect input", () => {
+    const invalidInput = {
+      installedCapacity: "e", // should be a number
+      powerPlantClass: "OnshoreWind",
+      location: "US",
+      capacityFactor: 0.25,
+      population2070: 1000000,
+      startYear: 2020,
+      lifeTimeYears: 30,
+      yearOfStudy: 2025,
+    };
+
+    expect(() => CalculateInput.parse(invalidInput)).toThrow();
+  });
 });
