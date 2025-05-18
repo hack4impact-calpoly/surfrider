c0b6c3ae6f0f1b14aecd14659a67678a58ab9664
Isha Varrier <ishavarrier@Ishas-MacBook-Air.local>
2025-05-18 10:19:32 -0700

    cleanup

--- src/app/ishavarrier/page.tsx
+++ src/app/ishavarrier/page.tsx
@@ -1,9 +1,17 @@
-import ResultsTable from "../../../src/components/results-table";
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-export default async function DemoPage() {
+export default function IshaVarrier() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="container mx-auto py-10">
-      <ResultsTable />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Count Value: {count}</h1>
+      <Button onClick={handleClick}>Click to Increment Count</Button>
     </div>
   );
 }
---
+++ src/app/results_table/page.tsx
@@ -0,0 +1,9 @@
+import ResultsTable from "../../components/results-table";
+
+export default async function Results() {
+  return (
+    <div className="container mx-auto py-10">
+      <ResultsTable />
+    </div>
+  );
+}
