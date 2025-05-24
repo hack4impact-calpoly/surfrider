192f55d53177153891cb0b492e9fdc13aeaf3e9b
Ryan Hu <ryanhu021@gmail.com>
2025-05-23 20:10:56 -0700

    chore: remove temp results_table route

--- src/app/results_table/page.tsx
+++
@@ -1,9 +0,0 @@
-import ResultsTable from "../../components/results-table";
-
-export default async function Results() {
-  return (
-    <div className="container mx-auto py-10">
-      <ResultsTable />
-    </div>
-  );
-}
